f78fe4637568ded8883d56b1ac75a7f331686047
XenonGa <111100012+XenonGa@users.noreply.github.com>
2023-08-25 11:16:50 +0800

    community

---
+++ front-project/src/components/Navbar.vue
@@ -0,0 +1,81 @@
+<template>
+    <!-- <nav>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link> |
+        <router-link to="/contact">Contact</router-link>
+    </nav>     -->
+    <el-menu
+    :default-active="activeIndex2"
+    class="el-menu-demo"
+    mode="horizontal"
+    @select="handleSelect"
+    background-color="#545c64"
+    text-color="#fff"
+    active-text-color="#ffd04b">
+  <el-menu-item index="1" class="menu-item-custom">处理中心</el-menu-item>
+  <el-submenu index="2">
+    <template slot="title" class="menu-item-custom">我的工作台</template>
+    <el-menu-item index="2-1">选项1</el-menu-item>
+    <el-menu-item index="2-2">选项2</el-menu-item>
+    <el-menu-item index="2-3">选项3</el-menu-item>
+    <el-submenu index="2-4">
+      <template slot="title">选项4</template>
+      <el-menu-item index="2-4-1">选项1</el-menu-item>
+      <el-menu-item index="2-4-2">选项2</el-menu-item>
+      <el-menu-item index="2-4-3">选项3</el-menu-item>
+    </el-submenu>
+  </el-submenu>
+  <el-menu-item index="3"><router-link to="/Community" class="menu-item-custom">Community</router-link></el-menu-item>
+  <el-menu-item index="4"><a href="https://www.ele.me" target="_blank" class="menu-item-custom">订单管理</a></el-menu-item>
+  <div class="menu-right-top">
+    <el-menu-item index="5" class="menu-avatar"><el-avatar :size="small" :src="circleUrl"></el-avatar>
+        <el-divider direction="vertical"></el-divider>
+        用户名</el-menu-item>
+    <el-menu-item index="6" class="menu-bell">
+        <el-badge class="item" :value="12" :max="99">
+            <i class="el-icon-message-solid"></i>
+        </el-badge>
+        
+    </el-menu-item>
+  </div>
+</el-menu>
+</template>
+  
+  <script>
+  export default {
+    name: 'Navbar',
+    data() {
+        return {
+            circleUrl: "https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png",
+        };
+    }
+  }
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped lang="scss">
+    nav {
+        background-color: #f5f5f5;
+        padding: 20px;
+    }
+    .menu-right-top {
+        display: flex;
+        justify-content: flex-end;
+    }
+    .menu-avatar
+    .menu-bell{
+        margin-left: 10px;
+    }
+    .menu-item-custom {
+        font-size: 16px;
+        font-weight: bold;
+    }
+    .item {
+        margin-top: 10px;
+        margin-right: 40px;
+        top: 12px;
+    }
+    .el-icon-message-solid {
+        margin-top: -43px;
+    }
+  </style>
--- front-project/src/views/HomeView.vue
+++ front-project/src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <Navbar />
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
     <el-button type="primary">主要按钮</el-button>
@@ -9,12 +10,13 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-
+import Navbar from '@/components/Navbar.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    HelloWorld,
+    Navbar
   }
 }
 </script>
