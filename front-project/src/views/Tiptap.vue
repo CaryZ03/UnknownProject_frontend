d1f527b4d7714528e214e0dd86c8d0636e0541bc
yizhiqianbi <850587960@qq.com>
2023-08-27 16:04:46 +0800

    commit

---
+++ front-project/src/views/Tiptap.vue
@@ -0,0 +1,522 @@
+<template>
+  <div v-if="editor">
+    <button
+      @click="editor.chain().focus().toggleBold().run()"
+      :disabled="!editor.can().chain().focus().toggleBold().run()"
+      :class="{ 'is-active': editor.isActive('bold') }"
+    >
+      bold
+    </button>
+    <button
+      @click="editor.chain().focus().toggleItalic().run()"
+      :disabled="!editor.can().chain().focus().toggleItalic().run()"
+      :class="{ 'is-active': editor.isActive('italic') }"
+    >
+      italic
+    </button>
+    <button
+      @click="editor.chain().focus().toggleStrike().run()"
+      :disabled="!editor.can().chain().focus().toggleStrike().run()"
+      :class="{ 'is-active': editor.isActive('strike') }"
+    >
+      strike
+    </button>
+    <button
+      @click="editor.chain().focus().toggleCode().run()"
+      :disabled="!editor.can().chain().focus().toggleCode().run()"
+      :class="{ 'is-active': editor.isActive('code') }"
+    >
+      code
+    </button>
+    <button @click="editor.chain().focus().unsetAllMarks().run()">
+      clear marks
+    </button>
+    <button @click="editor.chain().focus().clearNodes().run()">
+      clear nodes
+    </button>
+    <button
+      @click="editor.chain().focus().setParagraph().run()"
+      :class="{ 'is-active': editor.isActive('paragraph') }"
+    >
+      paragraph
+    </button>
+    <button
+      @click="editor.chain().focus().toggleHeading({ level: 1 }).run()"
+      :class="{ 'is-active': editor.isActive('heading', { level: 1 }) }"
+    >
+      h1
+    </button>
+    <button
+      @click="editor.chain().focus().toggleHeading({ level: 2 }).run()"
+      :class="{ 'is-active': editor.isActive('heading', { level: 2 }) }"
+    >
+      h2
+    </button>
+    <button
+      @click="editor.chain().focus().toggleHeading({ level: 3 }).run()"
+      :class="{ 'is-active': editor.isActive('heading', { level: 3 }) }"
+    >
+      h3
+    </button>
+    <button
+      @click="editor.chain().focus().toggleHeading({ level: 4 }).run()"
+      :class="{ 'is-active': editor.isActive('heading', { level: 4 }) }"
+    >
+      h4
+    </button>
+    <button
+      @click="editor.chain().focus().toggleHeading({ level: 5 }).run()"
+      :class="{ 'is-active': editor.isActive('heading', { level: 5 }) }"
+    >
+      h5
+    </button>
+    <button
+      @click="editor.chain().focus().toggleHeading({ level: 6 }).run()"
+      :class="{ 'is-active': editor.isActive('heading', { level: 6 }) }"
+    >
+      h6
+    </button>
+    <button
+      @click="editor.chain().focus().toggleBulletList().run()"
+      :class="{ 'is-active': editor.isActive('bulletList') }"
+    >
+      bullet list
+    </button>
+    <button
+      @click="editor.chain().focus().toggleOrderedList().run()"
+      :class="{ 'is-active': editor.isActive('orderedList') }"
+    >
+      ordered list
+    </button>
+    <button
+      @click="editor.chain().focus().toggleCodeBlock().run()"
+      :class="{ 'is-active': editor.isActive('codeBlock') }"
+    >
+      code block
+    </button>
+    <button
+      @click="editor.chain().focus().toggleBlockquote().run()"
+      :class="{ 'is-active': editor.isActive('blockquote') }"
+    >
+      blockquote
+    </button>
+    <button @click="editor.chain().focus().setHorizontalRule().run()">
+      horizontal rule
+    </button>
+    <button @click="editor.chain().focus().setHardBreak().run()">
+      hard break
+    </button>
+    <button
+      @click="editor.chain().focus().undo().run()"
+      :disabled="!editor.can().chain().focus().undo().run()"
+    >
+      undo
+    </button>
+    <button
+      @click="editor.chain().focus().redo().run()"
+      :disabled="!editor.can().chain().focus().redo().run()"
+    >
+      redo
+    </button>
+    <editor-content :editor="editor" />
+  </div>
+</template>
+  
+  <script>
+import { TiptapCollabProvider } from "@hocuspocus/provider";
+import CharacterCount from "@tiptap/extension-character-count";
+import Collaboration from "@tiptap/extension-collaboration";
+import CollaborationCursor from "@tiptap/extension-collaboration-cursor";
+import Highlight from "@tiptap/extension-highlight";
+import TaskItem from "@tiptap/extension-task-item";
+import TaskList from "@tiptap/extension-task-list";
+import StarterKit from "@tiptap/starter-kit";
+import { Editor, EditorContent } from "@tiptap/vue-2";
+import * as Y from "yjs";
+import MenuBar from "./MenuBar.vue";
+
+const getRandomElement = (list) => {
+  return list[Math.floor(Math.random() * list.length)];
+};
+
+export default {
+  components: {
+    EditorContent,
+    MenuBar,
+  },
+
+  data() {
+    return {
+      currentUser: JSON.parse(localStorage.getItem("currentUser")) || {
+        name: "pencil",
+        color: "#958DF1",
+      },
+      provider: null,
+      editor: null,
+      status: "connecting",
+      room: "room1",
+    };
+  },
+
+  mounted() {
+    const ydoc = new Y.Doc();
+    this.provider = new TiptapCollabProvider({
+      appId: "7j9y6m10",
+      name: this.room,
+      document: ydoc,
+    });
+
+    this.provider.on("status", (event) => {
+      this.status = event.status;
+    });
+
+    this.editor = new Editor({
+      extensions: [
+        StarterKit.configure({
+          history: false,
+        }),
+        Highlight,
+        TaskList,
+        TaskItem,
+        Collaboration.configure({
+          document: ydoc,
+        }),
+        CollaborationCursor.configure({
+          provider: this.provider,
+          user: this.currentUser,
+        }),
+        CharacterCount.configure({
+          limit: 10000,
+        }),
+      ],
+    });
+
+    localStorage.setItem("currentUser", JSON.stringify(this.currentUser));
+  },
+
+  methods: {
+    setName() {
+      const name = (window.prompt("Name") || "").trim().substring(0, 32);
+
+      if (name) {
+        return this.updateCurrentUser({
+          name,
+        });
+      }
+    },
+
+    updateCurrentUser(attributes) {
+      this.currentUser = { ...this.currentUser, ...attributes };
+      this.editor.chain().focus().updateUser(this.currentUser).run();
+
+      localStorage.setItem("currentUser", JSON.stringify(this.currentUser));
+    },
+
+    getRandomColor() {
+      return getRandomElement([
+        "#958DF1",
+        "#F98181",
+        "#FBBC88",
+        "#FAF594",
+        "#70CFF8",
+        "#94FADB",
+        "#B9F18D",
+      ]);
+    },
+
+    getRandomName() {
+      return getRandomElement([
+        "Lea Thompson",
+        "Cyndi Lauper",
+        "Tom Cruise",
+        "Madonna",
+        "Jerry Hall",
+        "Joan Collins",
+        "Winona Ryder",
+        "Christina Applegate",
+        "Alyssa Milano",
+        "Molly Ringwald",
+        "Ally Sheedy",
+        "Debbie Harry",
+        "Olivia Newton-John",
+        "Elton John",
+        "Michael J. Fox",
+        "Axl Rose",
+        "Emilio Estevez",
+        "Ralph Macchio",
+        "Rob Lowe",
+        "Jennifer Grey",
+        "Mickey Rourke",
+        "John Cusack",
+        "Matthew Broderick",
+        "Justine Bateman",
+        "Lisa Bonet",
+      ]);
+    },
+  },
+
+  beforeUnmount() {
+    this.editor.destroy();
+    this.provider.destroy();
+  },
+};
+</script>
+<style lang="scss">
+.editor {
+  background-color: #fff;
+  border: 3px solid #0d0d0d;
+  border-radius: 0.75rem;
+  color: #0d0d0d;
+  display: flex;
+  flex-direction: column;
+  max-height: 26rem;
+
+  &__header {
+    align-items: center;
+    background: #0d0d0d;
+    border-bottom: 3px solid #0d0d0d;
+    border-top-left-radius: 0.25rem;
+    border-top-right-radius: 0.25rem;
+    display: flex;
+    flex: 0 0 auto;
+    flex-wrap: wrap;
+    padding: 0.25rem;
+  }
+
+  &__content {
+    flex: 1 1 auto;
+    overflow-x: hidden;
+    overflow-y: auto;
+    padding: 1.25rem 1rem;
+    -webkit-overflow-scrolling: touch;
+  }
+
+  &__footer {
+    align-items: center;
+    border-top: 3px solid #0d0d0d;
+    color: #0d0d0d;
+    display: flex;
+    flex: 0 0 auto;
+    flex-wrap: wrap;
+    font-size: 12px;
+    font-weight: 600;
+    justify-content: space-between;
+    padding: 0.25rem 0.75rem;
+    white-space: nowrap;
+  }
+
+  /* Some information about the status */
+  &__status {
+    align-items: center;
+    border-radius: 5px;
+    display: flex;
+
+    &::before {
+      background: rgba(#0d0d0d, 0.5);
+      border-radius: 50%;
+      content: " ";
+      display: inline-block;
+      flex: 0 0 auto;
+      height: 0.5rem;
+      margin-right: 0.5rem;
+      width: 0.5rem;
+    }
+
+    &--connecting::before {
+      background: #616161;
+    }
+
+    &--connected::before {
+      background: #b9f18d;
+    }
+  }
+
+  &__name {
+    button {
+      background: none;
+      border: none;
+      border-radius: 0.4rem;
+      color: #0d0d0d;
+      font: inherit;
+      font-size: 12px;
+      font-weight: 600;
+      padding: 0.25rem 0.5rem;
+
+      &:hover {
+        background-color: #0d0d0d;
+        color: #fff;
+      }
+    }
+  }
+}
+
+/* Give a remote user a caret */
+.collaboration-cursor__caret {
+  border-left: 1px solid #0d0d0d;
+  border-right: 1px solid #0d0d0d;
+  margin-left: -1px;
+  margin-right: -1px;
+  pointer-events: none;
+  position: relative;
+  word-break: normal;
+}
+
+/* Render the username above the caret */
+.collaboration-cursor__label {
+  border-radius: 3px 3px 3px 0;
+  color: #0d0d0d;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 600;
+  left: -1px;
+  line-height: normal;
+  padding: 0.1rem 0.3rem;
+  position: absolute;
+  top: -1.4em;
+  user-select: none;
+  white-space: nowrap;
+}
+
+/* Basic editor styles */
+.tiptap {
+  > * + * {
+    margin-top: 0.75em;
+  }
+
+  ul,
+  ol {
+    padding: 0 1rem;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    line-height: 1.1;
+  }
+
+  code {
+    background-color: rgba(#616161, 0.1);
+    color: #616161;
+  }
+
+  pre {
+    background: #0d0d0d;
+    border-radius: 0.5rem;
+    color: #fff;
+    font-family: "JetBrainsMono", monospace;
+    padding: 0.75rem 1rem;
+
+    code {
+      background: none;
+      color: inherit;
+      font-size: 0.8rem;
+      padding: 0;
+    }
+  }
+
+  mark {
+    background-color: #faf594;
+  }
+
+  img {
+    height: auto;
+    max-width: 100%;
+  }
+
+  hr {
+    margin: 1rem 0;
+  }
+
+  blockquote {
+    border-left: 2px solid rgba(#0d0d0d, 0.1);
+    padding-left: 1rem;
+  }
+
+  hr {
+    border: none;
+    border-top: 2px solid rgba(#0d0d0d, 0.1);
+    margin: 2rem 0;
+  }
+
+  ul[data-type="taskList"] {
+    list-style: none;
+    padding: 0;
+
+    li {
+      align-items: center;
+      display: flex;
+
+      > label {
+        flex: 0 0 auto;
+        margin-right: 0.5rem;
+        user-select: none;
+      }
+
+      > div {
+        flex: 1 1 auto;
+      }
+    }
+  }
+}
+</style>
+  <style lang="scss">
+/* Basic editor styles */
+.tiptap {
+  > * + * {
+    margin-top: 0.75em;
+  }
+
+  ul,
+  ol {
+    padding: 0 1rem;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    line-height: 1.1;
+  }
+
+  code {
+    background-color: rgba(#616161, 0.1);
+    color: #616161;
+  }
+
+  pre {
+    background: #0d0d0d;
+    color: #fff;
+    font-family: "JetBrainsMono", monospace;
+    padding: 0.75rem 1rem;
+    border-radius: 0.5rem;
+
+    code {
+      color: inherit;
+      padding: 0;
+      background: none;
+      font-size: 0.8rem;
+    }
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+  }
+
+  blockquote {
+    padding-left: 1rem;
+    border-left: 2px solid rgba(#0d0d0d, 0.1);
+  }
+
+  hr {
+    border: none;
+    border-top: 2px solid rgba(#0d0d0d, 0.1);
+    margin: 2rem 0;
+  }
+}
+</style>
