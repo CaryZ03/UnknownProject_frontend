d97b4aab7c8e9116debab32009cfb7f0f99587e7
morty-idhyuajjcbdchjdehsr <326855092@qq.com>
2023-08-28 14:01:50 +0800

    1111
    新接上了一些接口

--- front-project/src/api/project.js
+++ front-project/src/api/project.js
@@ -1,5 +1,16 @@
 import request from "@/utils/request";
 
+//check_requirement_list
+export async function post_check_requirement_list(data) {
+    return request({
+        method: 'POST',
+        url:  `/project/check_requirement_list`,
+        data: data
+    })
+}
+
+
+
 
 // delete_project
 export async function post_delete_project(data) {
@@ -73,6 +84,34 @@ export async function post_check_project_list_team(data) {
     return request({
         method: 'POST',
         url:  `/project/check_project_list_team`,
+        data: data
+    })
+}
+
+//delete__requirement
+export async function post_delete_requirement(data) {
+    return request({
+        method: 'POST',
+        url:  `/project/delete_requirement`,
+        data: data
+    })
+}
+
+//change_profile_requirement
+export async function post_change_profile_requirement(data) {
+    return request({
+        method: 'POST',
+        url:  `/project/change_profile_requirement`,
+        data: data
+    })
+}
+
+
+//change_recycle_status
+export async function post_change_recycle_status(data) {
+    return request({
+        method: 'POST',
+        url:  `/project/change_recycle_status`,
         data: data
     })
 }
