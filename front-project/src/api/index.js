af1919b558f82aa7c1368c9d54a876844859da06
yizhiqianbi <850587960@qq.com>
2023-08-28 14:37:49 +0800

    commit

--- front-project/src/api/index.js
+++ front-project/src/api/index.js
@@ -2,10 +2,12 @@ import * as user from './user'
 import * as project from './project'
 import * as team from './team'
 import * as chat from './chat'
+import * as doc from './doc'
 
 export default{
     user,
     team,
     project,
-    chat
+    chat,
+    doc
 }
--- front-project/src/views/suggestion.js
+++ front-project/src/views/suggestion.js
@@ -6,7 +6,12 @@ import MentionList from './MentionList.vue'
 export default {
   items: ({ query }) => {
     return [
-      'Lea Thompson', 'Cyndi Lauper', 'Tom Cruise', 'Madonna', 'Jerry Hall', 'Joan Collins', 'Winona Ryder', 'Christina Applegate', 'Alyssa Milano', 'Molly Ringwald', 'Ally Sheedy', 'Debbie Harry', 'Olivia Newton-John', 'Elton John', 'Michael J. Fox', 'Axl Rose', 'Emilio Estevez', 'Ralph Macchio', 'Rob Lowe', 'Jennifer Grey', 'Mickey Rourke', 'John Cusack', 'Matthew Broderick', 'Justine Bateman', 'Lisa Bonet',
+      "Pencil",
+      "Pencil2",
+      "Xeon",
+      "WEI",
+      "CR",
+      "W",
     ].filter(item => item.toLowerCase().startsWith(query.toLowerCase())).slice(0, 5)
   },
 
