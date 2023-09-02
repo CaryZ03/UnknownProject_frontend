a5dfca37b21073a53d98de65e253ee18fc50b965
XenonGa <111100012+XenonGa@users.noreply.github.com>
2023-09-02 16:03:25 +0800

    notification updated

--- front-project/src/App.vue
+++ front-project/src/App.vue
@@ -1,5 +1,4 @@
 <template>
-  
   <div>
     <router-view></router-view>
     <!-- 在这里添加你的 HTML 模板代码 -->
@@ -25,41 +24,6 @@ export default {
       socket: null,
     };
   },
-  updated() {
-    this.socket = new WebSocket('ws://182.92.86.71:4514/ws/notification/receiver/7/');
-
-    this.socket.addEventListener('open', (event) => {
-      console.log('WebSocket connection opened:', event);
-
-      // 在这里可以发送用户认证信息，如 token 或其他信息
-      // this.socket.send(JSON.stringify({ "auth": "your_auth_token" }));
-      });
-      this.socket.addEventListener('message', (event) => {
-      const notification = JSON.parse(event.data);
-      console.log('Received notification:', notification);
-
-      // 将接收到的通知消息添加到数组中
-      this.notifications.push(notification);
-      const h = this.$createElement;
-
-        this.$notify({
-          title: '通知',
-          message: h('i', { style: 'color: teal'}, "你收到了一条通知消息")
-        });
-
-        this.$store.state.notificationRedNum += 1;
-    }); 
-
-    this.socket.addEventListener('close', (event) => {
-      console.log('WebSocket connection closed:', event);
-    });
-    },
-    beforeDestroy() {
-    // 在组件销毁之前，关闭 WebSocket 连接
-    if (this.socket !== null) {
-      this.socket.close();
-    }
-  },
   mounted() {
     this.$api.user
       .get_check_token()
@@ -72,7 +36,7 @@ export default {
           this.$store.state.curUserID = localStorage.getItem("curUserID");
           this.$store.state.curUserName = localStorage.getItem("curUserName");
 
-          
+
 
         } else {
           alert(res.data["errno"]);
@@ -83,35 +47,39 @@ export default {
       .catch((error) => {
         alert("error");
       });
-      this.socket = new WebSocket('ws://182.92.86.71:4514/ws/notification/receiver/7/');
+    this.socket = new WebSocket('ws://182.92.86.71:4514/ws/notification/receiver/7/');
 
     this.socket.addEventListener('open', (event) => {
       console.log('WebSocket connection opened:', event);
 
       // 在这里可以发送用户认证信息，如 token 或其他信息
       // this.socket.send(JSON.stringify({ "auth": "your_auth_token" }));
-      });
-      this.socket.addEventListener('message', (event) => {
+    });
+    this.socket.addEventListener('message', (event) => {
       const notification = JSON.parse(event.data);
       console.log('Received notification:', notification);
-
-      // 将接收到的通知消息添加到数组中
-      this.notifications.push(notification);
-      const h = this.$createElement;
+      const result = JSON.parse(notification);
+      console.log(typeof result.notification_receiver);
+      console.log(typeof parseInt(this.$store.state.curUserID));
+      if (result.notification_receiver === parseInt(this.$store.state.curUserID)) {
+        // 将接收到的通知消息添加到数组中
+        this.notifications.push(notification);
+        const h = this.$createElement;
 
         this.$notify({
           title: '通知',
-          message: h('i', { style: 'color: teal'}, "你收到了一条通知消息")
+          message: h('i', { style: 'color: teal' }, "你收到了一条通知消息")
         });
 
         this.$store.state.notificationRedNum += 1;
-    }); 
+      }
+    });
 
     this.socket.addEventListener('close', (event) => {
       console.log('WebSocket connection closed:', event);
     });
-    },
-    beforeDestroy() {
+  },
+  beforeDestroy() {
     // 在组件销毁之前，关闭 WebSocket 连接
     if (this.socket !== null) {
       this.socket.close();
@@ -127,7 +95,8 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 0; /* 取消或设置为 0 */
+  margin-top: 0;
+  /* 取消或设置为 0 */
   width: 100%;
   height: 100%;
 }
